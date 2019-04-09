27fefeb1da78fc2f9168594baffcf135167b6b37
Dev <dev@sourcetop.com>
2019-04-09 14:10:37 +0600

    Laravel From Scratch [Part 3] - Basic Routing & Controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'Nano Star Appliance')}}</title>
+    </head>
+    <body>
+        <h1>Welcome to Nano Star Appliance</h1>
+        <p>Home Page.</p>
+    </body>
+</html>
