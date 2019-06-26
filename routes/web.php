bf5c1ec0e5b8662cb2298d636be836ded4ea992d
Dev <dev@sourcetop.com>
2019-06-26 13:31:56 +0600

    Created ProductLists content type and table to DB

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
 Route::resource('posts', 'PostsController');
+Route::resources([
+    'posts' => 'PostsController',
+    'product_lists' => 'ProductListsController'
+]);
 Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index');
