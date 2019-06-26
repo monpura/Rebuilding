1e02bbdff4b78fec40bed6f06376f730634cb7e1
Dev <dev@sourcetop.com>
2019-06-26 14:01:01 +0600

    Created ProductLists content type and table to DB

--- app/Http/Controllers/ProductListsController.php
+++ app/Http/Controllers/ProductListsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\ProductList;
+
 class ProductListsController extends Controller
 {
     /**
--- app/ProductList.php
+++ app/ProductList.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductList extends Model
 {
-    //
+    // Table Name
+    protected $table = 'product_lists';
+    // Primary Key
+    public $primarykey = 'id';
+    // Timestamps
+    public $timestamps = true;
 }
