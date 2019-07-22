3977904ab66222ba0bd3a5b3888715ae4ea0fd32
Prasanta Mondal <pkmondalbca@gmail.com>
2019-07-22 23:26:47 +0600

    added deleted at field

--- app/User.php
+++ app/User.php
@@ -5,12 +5,18 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
-
+    use SoftDeletes;
+    
+    protected $dates = [
+        'deleted_at',
+    ];
     /**
      * The attributes that are mass assignable.
      *
